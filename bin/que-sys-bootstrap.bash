a69210a757cea469879dfc1c51cbd56bf1110436
Caleb Maclennan <caleb@alerque.com>
2013-08-15 15:57:23 +0300

    Assume bash for now.

--- bin/que-sys-bootstrap.bash
+++ bin/que-sys-bootstrap.bash
@@ -4,15 +4,6 @@
 grep -q -s "^Amazon Linux AMI" /etc/system-release && DISTRO=ala
 test -f /etc/pld-release && DISTRO=pld
 
-case $DISTRO in
-	ala|ubuntu)
-		SHELL=bash
-		;;
-	pld)
-		SHELL=zsh
-		;;
-esac
-
 # Make sure we have privs
 sudo -n true || exit 0
 
@@ -21,4 +12,4 @@ sudo -n true || exit 0
 # Check for network access
 
 # Fetch and run init script for this os
-$SHELL <(curl -S -L https://raw.github.com/alerque/que/master/bin/que-sys-init-${DISTRO}.${SHELL})
+bash <(curl -S -L https://raw.github.com/alerque/que/master/bin/que-sys-init-${DISTRO}.bash)
