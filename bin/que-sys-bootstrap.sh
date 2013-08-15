b906e538a5ecc933baa023c5e26feb5bc33436aa
Caleb Maclennan <caleb@alerque.com>
2013-08-15 15:48:40 +0300

    Fix syntax error

--- bin/que-sys-bootstrap.sh
+++ bin/que-sys-bootstrap.sh
@@ -5,7 +5,7 @@ grep -q -s "^Amazon Linux AMI" /etc/system-release && DISTRO=ala
 test -f /etc/pld-release && DISTRO=pld
 
 case $DISTRO in
-	ala)
+	ala|ubuntu)
 		SHELL=bash
 		;;
 	pld)
@@ -18,5 +18,7 @@ sudo -n true || exit 0
 
 # Make sure we have dependencies the init scripts will need
 
+# Check for network access
+
 # Fetch and run init script for this os
-$SHELL <(curl -S -L https://raw.github.com/alerque/que/master/bin/que-sys-init-$DISTRO.$SHELL)
+$SHELL <(curl -S -L https://raw.github.com/alerque/que/master/bin/que-sys-init-${DISTRO}.${SHELL})
