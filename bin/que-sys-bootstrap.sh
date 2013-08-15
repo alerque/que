f54199a8863baa313003169a8b2405f1d254ddd6
Caleb Maclennan <caleb@alerque.com>
2013-08-15 15:41:31 +0300

    Add latest tweaks and add init scripts for EC2 ALA systems.

--- bin/que-sys-bootstrap.sh
+++ bin/que-sys-bootstrap.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 # Detect distro
+grep -q -s "^Amazon Linux AMI" /etc/system-release && DISTRO=ala
+test -f /etc/pld-release && DISTRO=pld
+
+case $DISTRO in
+	ala)
+		SHELL=bash
+		;;
+	pld)
+		SHELL=zsh
+		;;
+esac
 
 # Make sure we have privs
+sudo -n true || exit 0
 
 # Make sure we have dependencies the init scripts will need
 
 # Fetch and run init script for this os
+$SHELL <(curl -S -L https://raw.github.com/alerque/que/master/bin/que-sys-init-$DISTRO.$SHELL)
---
+++ bin/que-sys-init-ala.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function flunk() {
+	echo "$*"
+	exit
+}
+
+# Make sure this is sane
+grep -q -s "^Amazon Linux AMI" /etc/system-release || flunk "Not an Amazon Linux AMI"
+
+# Figure out and set our hostname based on the EC2 instance name
+# (Uses que user that has read only permission to access tags on my EC2 account)
+HOSTNAME=$(ec2-describe-tags \
+		--aws-access-key AKIAIMSI2QP22SMUTUVQ \
+		--aws-secret-key 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 \
+		--filter "resource-type=instance" \
+		--filter "resource-id=$(ec2-metadata -i | cut -d ' ' -f2)" \
+		--filter "key=Name" | cut -f5)
+
+sudo sed -i -e "s/^HOSTNAME=.*/HOSTNAME=$HOSTNAME.alerque.com/g" /etc/sysconfig/network
+sudo hostname $HOSTNAME.alerque.com
+
+# Make sure the system is up to date with the repos
+sudo yum -y distribution-synchronization
+
+# Get packages that we're going to want across the board
+sudo yum -y install zsh subversion git tmux ctags pcre-tools
