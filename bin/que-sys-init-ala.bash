9f149557660a2c503ec7a85d2da5fe7e73dc702e
Caleb Maclennan <caleb@alerque.com>
2013-08-28 13:31:33 +0300

    Fix merge conflicts

--- bin/que-sys-init-ala.bash
+++ bin/que-sys-init-ala.bash
@@ -15,5 +15,8 @@ sudo hostname $HOSTNAME.alerque.com
 # Make sure the system is up to date with the repos
 sudo yum -y distribution-synchronization
 
+# Enable extra repros
+sudo sed -i 's/^enabled=0$/enabled=1/g' /etc/yum.repos.d/epel.repo
+
 # Get packages that we're going to want across the board
 sudo yum -y install ${BASEPACKAGES[@]}
