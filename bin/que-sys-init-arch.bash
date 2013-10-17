8482a98a3bd1b3de3b06eecb6c21b23a3cef6b52
Caleb Maclennan <caleb@alerque.com>
2013-10-17 17:30:58 -0600

    Configure pacman options

--- bin/que-sys-init-arch.bash
+++ bin/que-sys-init-arch.bash
@@ -4,4 +4,4 @@
 sudo pacman -Syu --needed --noconfirm
 
 # Make sure the basics every system is going to need are installed and updated
-sudo pacman -U --needed --noconfirm ${BASEPACKAGES[@]}
+sudo pacman -Su --needed --noconfirm ${BASEPACKAGES[@]}
