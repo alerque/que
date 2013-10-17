df1282684feec20e256191947a358acb071562a3
Caleb Maclennan <caleb@alerque.com>
2013-10-17 17:34:46 -0600

    Setup AUR on Archlinux

--- bin/que-sys-init-arch.bash
+++ bin/que-sys-init-arch.bash
@@ -5,3 +5,7 @@ sudo pacman -Syu --needed --noconfirm
 
 # Make sure the basics every system is going to need are installed and updated
 sudo pacman -S --needed --noconfirm ${BASEPACKAGES[@]}
+
+# Get AUR going
+pacman -S --needed --noconfirm base-devel
+bash <(curl aur.sh) -si aura --noconfirm --asroot
