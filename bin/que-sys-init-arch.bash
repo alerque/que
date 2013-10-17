a7341c0c6625fcf2bb695cff9ba24c1466fb128d
Caleb Maclennan <caleb@alerque.com>
2013-10-17 17:42:17 -0600

    Add haskell repos to pacman confs

--- bin/que-sys-init-arch.bash
+++ bin/que-sys-init-arch.bash
@@ -8,4 +8,10 @@ sudo pacman -S --needed --noconfirm ${BASEPACKAGES[@]}
 
 # Get AUR going
 pacman -S --needed --noconfirm base-devel
+
+grep -q haskell-core /etc/pacman.conf || (
+	sed -i 's/^\[extra\]$/[haskell-core]\nServer = http://xsounds.org/~haskell/core/$arch\n\n[extra]/g' /etc/pacman.conf
+	pacman -Syu
+)
+
 bash <(curl aur.sh) -si aura --noconfirm --asroot
