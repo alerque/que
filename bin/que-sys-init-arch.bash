76eba6f67ab639452238c687bc95c4a9236aef55
Caleb Maclennan <caleb@alerque.com>
2013-10-17 17:46:42 -0600

    Sign repo

--- bin/que-sys-init-arch.bash
+++ bin/que-sys-init-arch.bash
@@ -11,6 +11,7 @@ pacman -S --needed --noconfirm base-devel
 
 grep -q haskell-core /etc/pacman.conf || (
 	sed -i 's#^\[extra\]$#[haskell-core]\nServer = http://xsounds.org/~haskell/core/$arch\n\n[extra]#g' /etc/pacman.conf
+	pacman-key --lsign-key 4209170B
 	pacman -Syu
 )
 
