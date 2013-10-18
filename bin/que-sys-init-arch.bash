bf54766862ec6c7e1a7cf8a854f6f447645d7bdc
Caleb Maclennan <caleb@alerque.com>
2013-10-18 11:10:04 -0600

    Get key before signing.

--- bin/que-sys-init-arch.bash
+++ bin/que-sys-init-arch.bash
@@ -10,7 +10,7 @@ pacman -S --needed --noconfirm ${BASEPACKAGES[@]}
 
 # Network first net device on boot
 #systemctl enable dhcpcd@$(ip link show | grep ^2: | awk -F: '{gsub(/[ \t]+/, "", $2); print $2}').service
-
+e
 # Desktop stuff?
 # pacman -S --needed --noconfirm gnome xf86-video-nouveau nouveau-dri
 # systemctl enable gdm
@@ -27,8 +27,9 @@ lspci | grep -iq virtualbox && (
 pacman -S --needed --noconfirm base-devel
 grep -q haskell-core /etc/pacman.conf || (
 	sed -i 's#^\[extra\]$#[haskell-core]\nServer = http://xsounds.org/~haskell/core/$arch\n\n[extra]#g' /etc/pacman.conf
-	pacman -Sy --noconfirm
+	pacman-key -r 4209170B
 	pacman-key --lsign-key 4209170B
+	pacman -Sy --noconfirm
 )
 which aura || bash <(curl aur.sh) -si aura --noconfirm --asroot
 
