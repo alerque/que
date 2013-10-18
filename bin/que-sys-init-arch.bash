f7efc0b3073bf937d108ef687a0eb8ead5376538
Caleb Maclennan <caleb@alerque.com>
2013-10-18 11:17:19 -0600

    Set signature level

--- bin/que-sys-init-arch.bash
+++ bin/que-sys-init-arch.bash
@@ -26,7 +26,7 @@ lspci | grep -iq virtualbox && (
 # Get AUR going
 pacman -S --needed --noconfirm base-devel
 grep -q haskell-core /etc/pacman.conf || (
-	sed -i 's#^\[extra\]$#[haskell-core]\nServer = http://xsounds.org/~haskell/core/$arch\n\n[extra]#g' /etc/pacman.conf
+	sed -i 's#^\[extra\]$#[haskell-core]\nSigLevel = PackageRequired\nServer = http://xsounds.org/~haskell/core/$arch\n\n[extra]#g' /etc/pacman.conf
 	pacman-key -r 4209170B
 	pacman-key --lsign-key 4209170B
 	pacman -Sy --noconfirm
