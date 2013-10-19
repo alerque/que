c10cc93f7029cac6d6ead409d7e1acee068844b6
Caleb Maclennan <caleb@alerque.com>
2013-10-19 16:21:20 -0600

    Git complains about perms on .ssh/config

--- bin/que-home-bootstrap.bash
+++ bin/que-home-bootstrap.bash
@@ -28,7 +28,7 @@ cd $HOME
 # TODO: setup SSH auth credentials first
 test -d .ssh || mkdir .ssh
 test -f .ssh/id_rsa -f .ssh/github || ( umask 177 && curl --user caleb 'http://git.alerque.com/?p=caleb-private.git;a=blob_plain;f=.ssh/id_rsa;hb=HEAD' -o .ssh/id_rsa  'http://git.alerque.com/?p=caleb-private.git;a=blob_plain;f=.ssh/github;hb=HEAD' -o .ssh/github )
-grep -q github .ssh/config || echo -e "Host github.com\n\tIdentityFile ~/.ssh/github\n\tStrictHostKeyChecking no\n" >> .ssh/config
+grep -q github .ssh/config || (umask 177 && echo -e "Host github.com\n\tIdentityFile ~/.ssh/github\n\tStrictHostKeyChecking no\n" >> .ssh/config)
 
 eval $(ssh-agent)
 ssh-add .ssh/id_rsa
