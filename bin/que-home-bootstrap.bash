4e253a7bfb643c12852cb22ac8f1c192cbea0de6
Caleb Maclennan <caleb@alerque.com>
2013-10-19 16:53:13 -0600

    Check for proper unclobber capable vcsh and work around it if not.

--- bin/que-home-bootstrap.bash
+++ bin/que-home-bootstrap.bash
@@ -7,33 +7,34 @@ set -e
 
 # Meant to be a user space utility with optional sudo
 test $UID -eq 0 && exit
-
-# Make sure we have the tools
-# (otherwise we should be running a system init routine instead)
-which git > /dev/null
-which mr > /dev/null
-grep -q vcsh-unclobber $(which vcsh) || exit;
-false && {
-	mkdir -p ~/projects
-	test -d ~/projects/vcsh || git clone git@github.com:alerque/vcsh.git ~/projects/vcsh
-	#sudo ln -sf ~/projects/vcsh/vcsh /usr/local/bin/
-	#sudo cp ~/projects/vcsh/vcsh $(which vcsh)
-}
-
-# Clone the very base bit in...
 cd $HOME
 
-# Fetch private authentivation keys and such
+# If we don't have these tools, we shoudl be running que-sys-bootstrap.bash instead
+which git mr curl ssh-agent > /dev/null
 
-# TODO: setup SSH auth credentials first
+# If everything isn't just right with SSH keys and config for the next step, manually fetch them
 test -d .ssh || mkdir .ssh
-test -f .ssh/id_rsa -a -f .ssh/github || ( umask 177 && curl --user caleb 'http://git.alerque.com/?p=caleb-private.git;a=blob_plain;f=.ssh/id_rsa;hb=HEAD' -o .ssh/id_rsa  'http://git.alerque.com/?p=caleb-private.git;a=blob_plain;f=.ssh/github;hb=HEAD' -o .ssh/github )
-grep -q github .ssh/config || (umask 177 && echo -e "Host github.com\n\tIdentityFile ~/.ssh/github\n\tStrictHostKeyChecking no\n" >> .ssh/config)
+test -f .ssh/id_rsa -a -f .ssh/github && grep -q github .ssh/config || (
+	umask 177
+	curl --user caleb \
+		-o .ssh/id_rsa 'http://git.alerque.com/?p=caleb-private.git;a=blob_plain;f=.ssh/id_rsa;hb=HEAD' \
+		-o .ssh/github 'http://git.alerque.com/?p=caleb-private.git;a=blob_plain;f=.ssh/github;hb=HEAD' \
+		-o .ssh/config 'http://git.alerque.com/?p=caleb-private.git;a=blob_plain;f=.ssh/config;hb=HEAD'
+)
 
+# Now that we have keys, setup an agent so we don't keep getting prompted
 eval $(ssh-agent)
 ssh-add .ssh/id_rsa
 ssh-add .ssh/github
 
+# Make sure our vcsh has my anti-clobber hack, otherwise setup a local one for this operation
+which vcsh && grep -q vcsh-unclobber $(which vcsh) || {
+	mkdir -p ~/projects ;
+	test -f ~/projects/vcsh/vcsh || git clone git@github.com:alerque/vcsh.git ~/projects/vcsh ;
+	export PATH=~/projects/vcsh:$PATH ;
+}
+
+# If we don't have a config file for me, clone it manually so we have starting point
 test -f .mrconfig || vcsh clone git@github.com:alerque/que.git
 
 mr up
