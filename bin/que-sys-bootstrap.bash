3b26fa28362a7048ad8ddff28b3ad8396729d5d1
Caleb Maclennan <caleb@alerque.com>
2013-10-24 14:06:12 -0600

    Add commonly used shell extention

--- bin/que-sys-bootstrap.bash
+++ bin/que-sys-bootstrap.bash
@@ -56,7 +56,7 @@ case $DISTRO in
 	arch)
 		distro_pkg pcre-tools pcre
 		#distro_pkg flashplugin chromium-pepper-flash
-		distro_pkg gnome gnome{,-extra,-tweak-tool}
+		distro_pkg gnome gnome{,-extra,-tweak-tool,-shell-extension-maximus}
 		distro_pkg libreoffice libreoffice-{gnome,en-US,writer,calc,impress,math,draw} unoconv
 		distro_pkg cups cups cups-filters system-config-printer cups-pk-helper gsfonts gutenprint foomatic-{filters,db{,-engine,-nonfree}} hplip splix cups-pdf
 
@@ -70,6 +70,7 @@ case $DISTRO in
 		compile_desktop_pkg google-talkplugin
 		compile_desktop_pkg dropbox
 		compile_desktop_pkg google-chrome
+		compile_desktop_pkg gnome-shell-extension-maximus
 		:
 		;;
 	fedora)
