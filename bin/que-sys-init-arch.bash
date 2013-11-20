54cf22525d4d92f624ea153848f39075e6b03f43
Caleb Maclennan <caleb@alerque.com>
2013-11-20 23:10:37 +0200

    Drop sync comand that (unlike aura) operates on all repos including abs

--- bin/que-sys-init-arch.bash
+++ bin/que-sys-init-arch.bash
@@ -40,8 +40,8 @@ grep -q archlinuxfr /etc/pacman.conf || (
 which yaourt || pacman -Sy --needed --noconfirm yaourt aurvote customizepkg
 
 # Compile and install things not coming out of the distro main tree
-yaourt -Sa --noconfirm ${COMPILEBASEPACKAGES[@]}
-test "$ISDESKTOP" == '1' && yaourt -Sa --noconfirm ${COMPILEDESKTOPPACKAGES[@]}
+yaourt --noconfirm ${COMPILEBASEPACKAGES[@]}
+test "$ISDESKTOP" == '1' && yaourt --noconfirm ${COMPILEDESKTOPPACKAGES[@]}
 
 # TODO: Need to set root login and password auth options
 systemctl enable sshd
