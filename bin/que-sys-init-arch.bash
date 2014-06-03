bc53e4efdeb2105c7c987baf76c515098f4f7f22
Caleb Maclennan <caleb@alerque.com>
2014-06-03 12:43:56 +0300

    add debug option

--- bin/que-sys-init-arch.bash
+++ bin/que-sys-init-arch.bash
@@ -2,46 +2,53 @@
 
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
 
+# Make sure debug env var is either empty or prefix everything with a debug function
+: ${DEBUG:=}
+[[ -z $DEBUG ]] || DEBUG='debug'
+function debug () {
+	echo DEBUG: "$@"
+}
+
 # Make sure we're off on the right foot before we get to adding  keys
-pacman -Syu --needed --noconfirm haveged
+$DEBUG pacman -Syu --needed --noconfirm haveged
 haveged -w 1024
-pacman-key --init
-pacman-key --populate archlinux
+$DEBUG pacman-key --init
+$DEBUG pacman-key --populate archlinux
 pkill haveged
 
 # Freshen everything up
-pacman -Syu --needed --noconfirm
+$DEBUG pacman -Syu --needed --noconfirm
 
 # Remove anything that needs cleaning up first
-pacman -Rns --needed --noconfirm ${REMOVEPACKAGES[@]}
-pacman --noconfirm -Rns $(pacman -Qtdq)
+$DEBUG pacman -Rns --needed --noconfirm ${REMOVEPACKAGES[@]}
+$DEBUG pacman --noconfirm -Rns $(pacman -Qtdq)
 
 # Arch won't install gvim if vim is around, so to make the transition between package sets eaiser:
-is_opt $ISDESKTOP && pacman -R --noconfirm vim
+is_opt $ISDESKTOP && $DEBUG pacman -R --noconfirm vim
 
 # Make sure the basics every system is going to need are installed and updated
-pacman -S --needed --noconfirm ${BASEPACKAGES[@]}
-is_opt $ISDESKTOP && pacman -S --needed --noconfirm ${DESKTOPPACKAGES[@]}
+$DEBUG pacman -S --needed --noconfirm ${BASEPACKAGES[@]}
+is_opt $ISDESKTOP && $DEBUG pacman -S --needed --noconfirm ${DESKTOPPACKAGES[@]}
 
 # Detect VirtualBox guest and configure accordingly
 lspci | grep -iq virtualbox && (
-	pacman -S --needed --noconfirm virtualbox-guest-utils
+	$DEBUG pacman -S --needed --noconfirm virtualbox-guest-utils
 	echo -e "vboxguest\nvboxsf\nvboxvideo" > /etc/modules-load.d/virtualbox.conf
 	systemctl enable vboxservice.service
-	# pacman -S --needed --noconfirm xf86-video-vbox
+	# $DEBUG pacman -S --needed --noconfirm xf86-video-vbox
 )
 
 # Get AUR going
-pacman -S --needed --noconfirm base-devel
+$DEBUG pacman -S --needed --noconfirm base-devel
 
 grep -q archlinuxfr /etc/pacman.conf || (
 	sed -i 's#^\[extra\]$#[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/$arch\n\n[extra]#g' /etc/pacman.conf
 )
-which yaourt || pacman -Sy --needed --noconfirm yaourt aurvote customizepkg
+which yaourt || $DEBUG pacman -Sy --needed --noconfirm yaourt aurvote customizepkg
 
 # Compile and install things not coming out of the distro main tree
-yaourt --noconfirm -S --needed ${COMPILEBASEPACKAGES[@]}
-is_opt $ISDESKTOP && yaourt --noconfirm -S --needed ${COMPILEDESKTOPPACKAGES[@]}
+$DEBUG yaourt --noconfirm -S --needed ${COMPILEBASEPACKAGES[@]}
+is_opt $ISDESKTOP && $DEBUG yaourt --noconfirm -S --needed ${COMPILEDESKTOPPACKAGES[@]}
 
 # TODO: Need to set root login and password auth options
 systemctl enable sshd
@@ -51,7 +58,7 @@ systemctl enable cronie
 echo 'kernel.sysrq = 1' > /etc/sysctl.d/99-sysctl.conf
 
 if is_opt $ISDESKTOP; then
-	# pacman -S --needed --noconfirm xf86-video-nouveau nouveau-dri
+	# $DEBUG pacman -S --needed --noconfirm xf86-video-nouveau nouveau-dri
 	systemctl enable gdm
 	systemctl enable cups
 	systemctl enable NetworkManager
