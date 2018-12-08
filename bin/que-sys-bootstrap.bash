b6162a80bc185ae1dd94f0f055b9e7b3db79d4d7
Caleb Maclennan <caleb@alerque.com>
2018-12-08 14:27:09 +0300

    Don't die if etckeeper remote is already set

--- bin/que-sys-bootstrap.bash
+++ bin/que-sys-bootstrap.bash
@@ -202,7 +202,7 @@ if command -v etckeeper; then
 	(
 	cd /etc 
 	etckeeper vcs status || etckeeper init
-	etckeeper vcs remote add origin gitlab@gitlab.alerque.com:hosts/$HOSTNAME.git
+	etckeeper vcs remote add origin gitlab@gitlab.alerque.com:hosts/$HOSTNAME.git ||:
 	# TODO: set default push remote
 	etckeeper commit "End of que-sys-bootstrap.bash run"
 	)
