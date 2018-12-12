51ed5a75e7aa5012d13a6aa0a58450c60036f532
Caleb Maclennan <caleb@alerque.com>
2018-12-12 11:54:57 +0300

    Refactor home bootstrap to clone early and repeat less

--- bin/que-home-bootstrap.bash
+++ bin/que-home-bootstrap.bash
@@ -23,43 +23,38 @@ test -d .ssh || mkdir .ssh ; chmod 750 .ssh
 (umask 177
 	test -f .ssh/id_rsa -a -f .ssh/github && grep -q github .ssh/config || (
         curl --request GET --header "Private-Token: $(read -s -p 'Gitlab Private-Token: ' && echo $REPLY)" \
-            -o .ssh/id_rsa      'https://gitlab.alerque.com/caleb/que-secure/raw/master/.ssh%2Fid_rsa' \
-            -o .ssh/id_rsa.pub  'https://gitlab.alerque.com/caleb/que-secure/raw/master/.ssh%2Fid_rsa.pub' \
-            -o .ssh/github      'https://gitlab.alerque.com/caleb/que-secure/raw/master/.ssh%2Fgithub' \
             -o .ssh/gitlab      'https://gitlab.alerque.com/caleb/que-secure/raw/master/.ssh%2Fgitlab' \
             -o .ssh/known_hosts 'https://gitlab.alerque.com/caleb/que-secure/raw/master/.ssh%2Fknown_hosts' \
             -o .ssh/config      'https://gitlab.alerque.com/caleb/que-secure/raw/master/.ssh%2Fconfig'
 	)
-    grep -q github.com .ssh/config || fail "Invalid creds, got garbage files"
-	test -f .ssh/authorized_keys || cp .ssh/{id_rsa.pub,authorized_keys}
+    grep -q github.com .ssh/config ||
+        fail "Invalid creds, got garbage files"
 )
 
-# Now that we have keys, setup an agent so we don't keep getting prompted
 eval $(ssh-agent)
+ssh-add .ssh/gitlab
+
+# Rename repository if it exists under old name
+test -d .config/vcsh/repo.d/caleb-private.git &&
+    mv .config/vcsh/repo.d/{caleb-private,que-secure}.git
+
+# mr would clone this, but it needs this to clone other things and this needs manual care on first setup
+test -d .config/vcsh/repo.d/que-secure.git &&
+    vcsh que-secure pull ||
+    vcsh clone git@gitlab.alerque.com:caleb/que-secure.git que-secure
+chmod 600 .ssh/{config,authorized_keys} $(grep 'PRIVATE KEY' -Rl .ssh)
+
 ssh-add .ssh/id_rsa
 ssh-add .ssh/github
-ssh-add .ssh/gitlab
+ssh-add .ssh/aur
 
 grep -q 'hook pre-merge' $(which vcsh) ||
     fail "VCSH version too old, does not have required pre-merge hook system"
 
-test -d .config/vcsh/repo.d/que.git || vcsh clone git@github.com:alerque/que.git que
-
-# For the sake of un-updated que repos, get hooks we want to use on mr's initial clones
-if test -d .config/vcsh; then
-    mkdir -p .config/vcsh/hooks-enabled
-    test -f .config/vcsh/hooks-enabled/pre-merge-unclobber ||
-        curl -L -o .config/vcsh/hooks-enabled/pre-merge-unclobber $STRAP_URL/.config/vcsh/hooks-enabled/pre-merge-unclobber
-    test -f .config/vcsh/hooks-enabled/post-merge-unclobber ||
-        curl -L -o .config/vcsh/hooks-enabled/post-merge-unclobber $STRAP_URL/.config/vcsh/hooks-enabled/post-merge-unclobber
-    chmod +x .config/vcsh/hooks-enabled/{pre,post}-merge-unclobber
-fi
-
-# mr would clone this, but it needs this to clone other things and this needs manual care on first setup
-vcsh clone git@gitlab.alerque.com:caleb/que-secure.git caleb-private
-
-# Patch up SSH private key permissions
-chmod 600 .ssh/{config,authorized_keys} $(grep 'PRIVATE KEY' -Rl .ssh)
+# Get or update man repo that has mr configs
+test -d .config/vcsh/repo.d/que.git &&
+    vcsh que pull ||
+    vcsh clone git@github.com:alerque/que.git que
 
 # checkout everything else
 mr co
