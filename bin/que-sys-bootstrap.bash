619c1270cd722ff2111a84406c028b589dbad97d
Caleb Maclennan <caleb@alerque.com>
2019-04-23 10:16:28 +0300

    Add bridge for NodeJS plugins in Neovim to desktops

--- bin/que-sys-bootstrap.bash
+++ bin/que-sys-bootstrap.bash
@@ -229,7 +229,7 @@ case $DISTRO in
 		distro_pkg libreoffice libreoffice-fresh{,-tr,-ru} unoconv
 		distro_pkg lightdm lightdm{,-gtk-greeter{,-settings}}
 		distro_pkg neomutt neomutt goobook-git
-		distro_pkg neovim {,python{,2}-}neovim
+		distro_pkg neovim {,{python{,2},nodejs}-}neovim
 		distro_pkg networkmanager networkmanager network-manager-applet
 		distro_pkg nextcloud-client nextcloud-client python-nautilus
 		distro_pkg pcregrep pcre
