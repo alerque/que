7d70866bb4ad7ed71d15c15c4e707eafd9e8b6d9
Caleb Maclennan <caleb@alerque.com>
2019-11-27 11:27:28 +0300

    Clear UI cache after hot-patching Firefox

--- bin/patch-firefox.sh
+++ bin/patch-firefox.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
 sudo perl -i -pne 's/reserved="true"/               /g' /usr/lib/firefox/browser/omni.ja
+find ~/.cache/mozilla/firefox -type d -name startupCache | xargs rm -rf
