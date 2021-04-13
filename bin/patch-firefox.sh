a4ebe633ef5bd2f538d10dec37380192737fe618
Caleb Maclennan <caleb@alerque.com>
2021-04-13 06:54:53 +0300

    Drop firefox script moved to que-firefox

--- bin/patch-firefox.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-sudo perl -i -pne 's/reserved="true"/               /g' /usr/lib/firefox/browser/omni.ja
-find ~/.cache/mozilla/firefox -type d -name startupCache | xargs rm -rf
