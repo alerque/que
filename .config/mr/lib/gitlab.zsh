91a111a9f561ec831f4ed6bb6809dc54e9196233
Caleb Maclennan <caleb@alerque.com>
2022-10-31 13:42:06 +0300

    Cleanup gitlab repo caching

--- .config/mr/lib/gitlab.zsh
+++ .config/mr/lib/gitlab.zsh
@@ -1,36 +1,34 @@
 set -eo pipefail 
 
-test -v host
-
-cachefile=/tmp/gitlab-repos-cache-$host
-
 function flunk () {
 	echo "$1" >&2
 	exit 1
 }
 
 function project_list_cache () (
-	test -s ${cachefile} || exit 1
-	test $(date +%s -r ${cachefile}) -ge $(date +%s --date "60 min ago") || exit 1
-	cat ${cachefile}
+	test -s $cachefile || exit 1
+	test $(date +%s -r $cachefile) -ge $(date +%s --date "60 min ago") || exit 1
+	cat $cachefile
 )
 
 function fetch_project_list () {
-	yq --exit-status ".hosts[\"$host\"].token" ~/.config/glab-cli/config.yml || flunk "No API key for host $host"
-	glab repo list --all --per-page 1000 |
+	yq -re ".hosts[\"$host\"].token" ~/.config/glab-cli/config.yml || flunk "No API key for host $host"
+	GLAB_HOST=$host glab repo list --all --per-page 1000 |
 		awk '/^[^ ]/ { print $1 }' |
-		sort
+		sort |
+		tee $cachefile
 }
 
 function list_all_projects () {
-	project_list_cache || fetch_project_list |
-		tee ${cachefile}
+	project_list_cache || fetch_project_list
 }
 
 function list_group_projects () {
-	test -v 1
+	host=${1:==gitlab.alerque.com}
+	group=${2:==$USER}
+	cachefile=/tmp/gitlab-repos-cache-$host
 	list_all_projects |
-		grep -ef "^$1/" |
+		grep -Fe "^$group/" |
 		while read project; do
 			cat <<- EOF
 				[$HOME/projects/${project}]
